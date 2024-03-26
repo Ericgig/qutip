61c945970038dfe6381e0acc7713f86b40beb58a
Eric Giguere <eric.giguere@gmail.com>
2024-03-27 08:45:46 +0900

    Add scipy print

--- qutip/tests/test_scipy.py
+++ qutip/tests/test_scipy.py
@@ -3,22 +3,11 @@ import scipy
 import pytest
 import scipy.sparse
 
-"""@pytest.mark.parametrize("N", [10, 100])
-def test_sparse_eigen_eye(N):
-    matrix = scipy.sparse.csr_matrix(np.eye(10))
-    out = scipy.sparse.linalg.eigs(matrix, 4)
+import sys
+print(scipy.__version__, np.__version__, sys.version_info)
+scipy.show_config()
 
-@pytest.mark.parametrize("N", [10, 100])
-def test_sparse_eigen(N):
-    matrix = scipy.sparse.random(N, N, format="csr", dtype=np.double, random_state=1)
-    out = scipy.sparse.linalg.eigs(matrix, 4)
-"""
 @pytest.mark.parametrize("N", [10, 100])
 def test_sparse_eigen(N):
     matrix = scipy.sparse.random(N, N, format="csr", dtype=np.complex128, random_state=2)
     out = scipy.sparse.linalg.eigs(matrix, 4)
-"""
-@pytest.mark.parametrize("N", [10, 100])
-def test_sparse_svd(N):
-    matrix = scipy.sparse.random(N, N, format="csr", dtype=np.complex128, random_state=3)
-    out = scipy.sparse.linalg.svds(matrix, 4)"""
