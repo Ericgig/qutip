a9890124a6ee0ef4dcd35817dc4424e490eca48b
Eric Giguere <eric.giguere@gmail.com>
2024-03-26 09:51:40 +0900

    scipy should segfault

--- qutip/tests/test_scipy.py
+++ qutip/tests/test_scipy.py
@@ -1,21 +1,24 @@
 import numpy as np
 import scipy
 import pytest
+import scipy.sparse
+
+@pytest.mark.parametrize("N", [10, 100])
+def test_sparse_eigen_eye(N):
+    matrix = scipy.sparse.csr_matrix(np.eye(10))
+    out = scipy.sparse.linalg.eigs(matrix, 4)
 
 @pytest.mark.parametrize("N", [10, 100])
 def test_sparse_eigen(N):
-    import scipy.sparse
     matrix = scipy.sparse.random(N, N, format="csr", dtype=np.double)
     out = scipy.sparse.linalg.eigs(matrix, 4)
 
 @pytest.mark.parametrize("N", [10, 100])
 def test_sparse_eigen(N):
-    import scipy.sparse
     matrix = scipy.sparse.random(N, N, format="csr", dtype=np.complex128)
     out = scipy.sparse.linalg.eigs(matrix, 4)
 
 @pytest.mark.parametrize("N", [10, 100])
 def test_sparse_svd(N):
-    import scipy.sparse
     matrix = scipy.sparse.random(N, N, format="csr", dtype=np.complex128)
     out = scipy.sparse.linalg.svds(matrix, 4)
