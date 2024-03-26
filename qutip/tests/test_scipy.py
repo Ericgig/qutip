d5d6d4a42cf5251f459685445c075165630ef3bb
Eric Giguere <eric.giguere@gmail.com>
2024-03-27 08:32:26 +0900

    Test complex

--- qutip/tests/test_scipy.py
+++ qutip/tests/test_scipy.py
@@ -3,7 +3,7 @@ import scipy
 import pytest
 import scipy.sparse
 
-@pytest.mark.parametrize("N", [10, 100])
+"""@pytest.mark.parametrize("N", [10, 100])
 def test_sparse_eigen_eye(N):
     matrix = scipy.sparse.csr_matrix(np.eye(10))
     out = scipy.sparse.linalg.eigs(matrix, 4)
@@ -12,13 +12,13 @@ def test_sparse_eigen_eye(N):
 def test_sparse_eigen(N):
     matrix = scipy.sparse.random(N, N, format="csr", dtype=np.double, random_state=1)
     out = scipy.sparse.linalg.eigs(matrix, 4)
-
+"""
 @pytest.mark.parametrize("N", [10, 100])
 def test_sparse_eigen(N):
     matrix = scipy.sparse.random(N, N, format="csr", dtype=np.complex128, random_state=2)
     out = scipy.sparse.linalg.eigs(matrix, 4)
-
+"""
 @pytest.mark.parametrize("N", [10, 100])
 def test_sparse_svd(N):
     matrix = scipy.sparse.random(N, N, format="csr", dtype=np.complex128, random_state=3)
-    out = scipy.sparse.linalg.svds(matrix, 4)
+    out = scipy.sparse.linalg.svds(matrix, 4)"""
